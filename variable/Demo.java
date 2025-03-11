3874340b0dfdee30515521438dfaff7186b07089
gokulraj.sivaraj <gokulraj.sivaraj@zoifintech.com>
2025-03-11 20:27:08 +0530

    i have add samples

--- variable/Demo.java
+++ variable/Demo.java
@@ -1,8 +1,7 @@
-package samples;
 public class Demo {
     public void add(int a,int b){
         int c = a+b;
-        System.out.println(c+c);
+        System.out.println(c);
     }
     public static void main(String args[]) {
         System.out.println("Hello world");
